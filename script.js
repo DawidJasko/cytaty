c8ced65c56a513fb9ac5d6e4fdf18f80983400bc
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2023-01-13 01:08:39 +0100

    add local api quotes

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-let apiQuotes = [];
+// let apiQuotes = [];
 
-// Get Quotes From API
-
-async function getQuotes() {
-  const apiUrl = 'https://type.fit./api/quotes';
-
-  try {
-    const response = await fetch(apiUrl);
-    apiQuotes = await response.json();
-    console.log(apiQuotes);
-  } catch (error) {
-    alert;
-    // Catch Error Here
-  }
+//Show New Quote
+function newQuote() {
+  // Pick a random quote from apiQuotes array
+  const quote = localQuote[Math.floor(Math.random() * localQuote.length)];
+  console.log(quote);
 }
 
-getQuotes();
+// // Get Quotes From API
+
+// async function getQuotes() {
+//   const apiUrl = 'https://type.fit./api/quotes';
+
+//   try {
+//     const response = await fetch(apiUrl);
+//     apiQuotes = await response.json();
+//     console.log(apiQuotes);
+//   } catch (error) {
+//     alert;
+//     // Catch Error Here
+//   }
+// }
+
+// getQuotes();
+
+newQuote();
