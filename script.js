7ca6869aae439b27ec890c8a8881177849bbbf30
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2023-01-14 00:51:58 +0100

    end project

--- script.js
+++ script.js
@@ -3,11 +3,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+//Show Loading
+
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+//Hide Loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 //Show New Quote
 function newQuote() {
+  loading();
   // Pick a random quote from apiQuotes array
   const quote = localQuote[Math.floor(Math.random() * localQuote.length)];
   //Check if Author is blank and replese it with 'Unknow'
@@ -23,18 +38,21 @@ function newQuote() {
     quoteText.classList.remove('long-quote');
   }
 
+  // Set Quote, Hide Loaderr
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get Quotes From API
 
 async function getQuotes() {
+  loading();
   const apiUrl = 'https://type.fit./api/quotes';
 
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
-    console.log(apiQuotes);
+    newQuote();
   } catch (error) {
     alert;
     // Catch Error Here
@@ -53,6 +71,3 @@ twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuotes();
-
-// Local
-newQuote();
